47097712b908830a9499a1cd8547fc97e7c45fe0
Lena Hou <lenahou2013@gmail.com>
2021-02-10 23:18:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,9 +23,12 @@ def self.clear_all
   @@all.clear
 end
 
+# This is an instance method
+# self in here refers to the current object itself
+# This method will be called on immediately when an instance is instantiated
+# If you call dog.save manually, it will duplicate the dog instance
 def save
   @@all << self
 end
-
 
 end
